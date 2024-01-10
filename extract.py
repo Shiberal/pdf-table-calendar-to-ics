cefb57f0dfb7ec1796f81bbf2936b3e30d35881d
Francesco <francesco.grelli98@gmail.com>
2024-01-10 10:58:14 +0100

    Aggiornato per il calendario più recente.

--- extract.py
+++ extract.py
@@ -2,10 +2,27 @@ import tabula
 
 def extract_table_from_pdf(pdf_path, output_csv_path):
     # Read the PDF file and extract tables
-    tables = tabula.read_pdf(pdf_path, pages='all', multiple_tables=True)
+    tables = tabula.read_pdf(pdf_path, pages='all' )
 
     # Define the header for the CSV files
-    header = ["Day", "Date", "Start Time", "End Time", "Duration", "Subject", "Empty", "Location", "Instructor"]
+    #header = ["Day", "Date", "Start Time", "End Time", "Duration", "Subject", "Empty", "Location", "Instructor"]
+
+    #extract header from the second row in table
+    header = tables[0].iloc[0]
+    header = header.dropna()
+    header = header.tolist()
+    
+    #replace for each element in header the \r with " " with map
+    header = list(map(lambda x: x.replace("\r", " "), header))
+    #append to the array from array start "weekday"
+    header.insert(0, "Giorno Settimana")
+
+    #remove the first row
+    tables[0] = tables[0].iloc[1:]
+    tables[0] = tables[0].reset_index(drop=True)
+
+    
+    print(f"Header: {header}")
 
     # Save each table as a separate CSV file with the header
     for i, table in enumerate(tables):
@@ -17,7 +34,7 @@ def extract_table_from_pdf(pdf_path, output_csv_path):
             table.columns = header + [f"Column_{j}" for j in range(len(table.columns) - len(header))]
 
             # Save the table to CSV
-            table.to_csv(csv_path, index=False)
+            table.to_csv(csv_path, index=False, header=True)
             print(f"Table {i + 1} extracted and saved to {csv_path}")
         else:
             print(f"Table {i + 1} is empty, skipping.")
--- ical.py
+++ ical.py
@@ -12,13 +12,13 @@ def csv_to_ics(input_csv, output_ics='output.ics'):
         for row in reader:
             #map the row 
             #event date is 21/12/2023, 
-            event_date = datetime.strptime(row[1], '%d/%m/%Y')
+            event_date = datetime.strptime(row[1], '%d/%m/%y')
             #replace , with : to get  9,00 to 9:00
             start_time = datetime.strptime(row[2].replace(",",":"), '%H:%M')
             end_time = datetime.strptime(row[3].replace(",",":"), '%H:%M')
-            subject = row[5]
-            location = row[7]
-            organizer = row[8]
+            subject = row[7]
+            location = row[9]
+            organizer = row[10]
 
             #replace in event date the day
             #event_date = event_date.replace(day=event_date.day-1)
